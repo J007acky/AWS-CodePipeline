e3b906d21dbd00d56a664648c07839f2af72b6da
J007acky <111886827+J007acky@users.noreply.github.com>
2024-12-27 13:28:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "subs_id" {
   description = "Subscription ID"
   type = string
 }
+variable "tenant_id" {
+  description = "Tenant ID"
+  type = string
+}
+variable "client_id" {
+  description = "Client ID"
+  type = string
+}
+variable "client_secret" {
+  description = "Client Secret"
+  type = string
+}
