6a0180be512189884470377b305319d8ee5130d7
J007acky <111886827+J007acky@users.noreply.github.com>
2024-12-27 16:44:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,13 +5,16 @@ variable "subs_id" {
 }
 variable "tenant_id" {
   description = "Tenant ID"
+  default = "2ce14b3a-00b4-45c9-b725-87d89bd6532f"
   type = string
 }
 variable "client_id" {
   description = "Client ID"
+  default= "d6256461-f586-4afa-857f-305bd513ff20"
   type = string
 }
 variable "client_secret" {
   description = "Client Secret"
+  default="OWc8Q~.H5OxJ4edxSbAS~6Jj8Ifsrv9a6dap8bjC"
   type = string
 }
