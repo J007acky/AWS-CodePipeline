b7907eec045b5981de168041656ebb85e6802fc9
J007acky <111886827+J007acky@users.noreply.github.com>
2024-12-27 13:27:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,9 @@ terraform {
 provider "azurerm" {
     features {}
     subscription_id = var.subs_id
+    tenant_id = var.tenant_id
+    client_id = var.client_id
+    client_secret = var.client_secret
 }
 
 # Resoure Group
