6866baaca221ced9154e471ae48189fd674b569e
J007acky <111886827+J007acky@users.noreply.github.com>
2024-12-27 13:59:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@
   provider "azurerm" {
       features {}
       subscription_id = var.subs_id
-#      tenant_id = var.tenant_id
+      tenant_id = var.tenant_id
 #     client_id = var.client_id
 #    client_secret = var.client_secret
   }
