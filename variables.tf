d91297e9b198d71cdf5ec2ba4cb12087e4430709
J007acky <111886827+J007acky@users.noreply.github.com>
2024-12-30 10:18:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
 variable "subs_id" {
   description = "Subscription ID"
-  default = "71dd8aa2-d476-436b-bf75-61b8ae29cd69"
+#  default = "71dd8aa2-d476-436b-bf75-61b8ae29cd69"
   type = string
 }
 variable "tenant_id" {
   description = "Tenant ID"
-  default = "2ce14b3a-00b4-45c9-b725-87d89bd6532f"
+#  default = "2ce14b3a-00b4-45c9-b725-87d89bd6532f"
   type = string
 }
 variable "client_id" {
   description = "Client ID"
-  default= "d6256461-f586-4afa-857f-305bd513ff20"
+#  default= "d6256461-f586-4afa-857f-305bd513ff20"
   type = string
 }
 variable "client_secret" {
   description = "Client Secret"
-  default="OWc8Q~.H5OxJ4edxSbAS~6Jj8Ifsrv9a6dap8bjC"
+#  default="OWc8Q~.H5OxJ4edxSbAS~6Jj8Ifsrv9a6dap8bjC"
   type = string
 }
