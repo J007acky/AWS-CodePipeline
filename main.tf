37d9d2e95c92a93b4f5599d8f58a4b99975dde08
J007acky <111886827+J007acky@users.noreply.github.com>
2024-12-30 10:39:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,31 +25,31 @@
   }
 
   # # Storage Account
-  # resource "azurerm_storage_account" "testStorageAccount" {
-  #   name                     = "testStorage"
-  #   resource_group_name      = azurerm_resource_group.RahulRG.name
-  #   location                 = azurerm_resource_group.RahulRG.location
-  #   account_tier             = "Standard"
-  #   account_replication_type = "LRS"
-  # }
+   resource "azurerm_storage_account" "testStorageAccount" {
+     name                     = "testStorage"
+     resource_group_name      = azurerm_resource_group.RahulRG.name
+     location                 = azurerm_resource_group.RahulRG.location
+     account_tier             = "Standard"
+     account_replication_type = "LRS"
+   }
 
 
-  # # Service Plan
-  # resource "azurerm_app_service_plan" "testServicePlan" {
-  #   name                = "azure-functions-test-service-plan"
-  #   location            = azurerm_resource_group.RahulRG.location
-  #   resource_group_name = azurerm_resource_group.RahulRG.name
-  #   os_type = "Linux"
-  #   sku_name = "Y1"
-  # }
+   # Service Plan
+   resource "azurerm_app_service_plan" "testServicePlan" {
+     name                = "azure-functions-test-service-plan"
+     location            = azurerm_resource_group.RahulRG.location
+     resource_group_name = azurerm_resource_group.RahulRG.name
+     os_type = "Linux"
+     sku_name = "Y1"
+   }
 
 
-  # # Azure Function
-  # resource "azurerm_function_app" "example" {
-  #   name                       = "test-azure-functions"
-  #   location                   = azurerm_resource_group.RahulRG.location
-  #   resource_group_name        = azurerm_resource_group.RahulRG.name
-  #   app_service_plan_id        = azurerm_app_service_plan.testServicePlan.id
-  #   storage_account_name       = azurerm_storage_account.testStorageAccount.name
-  #   storage_account_access_key = azurerm_storage_account.testStorageAccount.primary_access_key
-  # }
+   # Azure Function
+   resource "azurerm_function_app" "example" {
+     name                       = "testazurefunctions123"
+     location                   = azurerm_resource_group.RahulRG.location
+     resource_group_name        = azurerm_resource_group.RahulRG.name
+     app_service_plan_id        = azurerm_app_service_plan.testServicePlan.id
+     storage_account_name       = azurerm_storage_account.testStorageAccount.name
+     storage_account_access_key = azurerm_storage_account.testStorageAccount.primary_access_key
+   }
