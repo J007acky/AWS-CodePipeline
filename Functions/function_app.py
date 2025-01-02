9a475263dcbfe839cef24803bf6983369a75f677
J007acky <111886827+J007acky@users.noreply.github.com>
2025-01-02 11:09:42 +0530

    Update function_app.py

--- Functions/function_app.py
+++ Functions/function_app.py
@@ -1,7 +1,7 @@
 import azure.functions as func
 import logging
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="http_trigger",methods=["GET"])
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
