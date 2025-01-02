992a1234c819bc0c41a058785e9e77b2f633f21f
J007acky <111886827+J007acky@users.noreply.github.com>
2025-01-02 11:46:05 +0530

    Update variables.tf

--- IAC/variables.tf
+++ IAC/variables.tf
@@ -1,25 +1,25 @@
 variable "subs_id" {
   description = "Subscription ID"
-  default = "8a228cff-023f-42af-818d-51a84a828d46"
+#  default = "8a228cff-023f-42af-818d-51a84a828d46"
   type = string
 }
 variable "tenant_id" {
   description = "Tenant ID"
-  default = "46118836-cde0-431b-bb1e-b6ab7887003c"
+#  default = "46118836-cde0-431b-bb1e-b6ab7887003c"
   type = string
 }
 variable "client_id" {
   description = "Client ID"
-  default= "20c516e5-a70b-4ebc-bbed-4f2dd86c3141"
+#  default= "20c516e5-a70b-4ebc-bbed-4f2dd86c3141"
   type = string
 }
 variable "client_secret" {
   description = "Client Secret"
-  default="bH18Q~9cOEq7OH1m3jxXHSOwg_PjSYn38IM9Saik"
+#  default="bH18Q~9cOEq7OH1m3jxXHSOwg_PjSYn38IM9Saik"
   type = string
 }
 variable "blob_url" {
 description = "Blob link for Function Deployment"
-default="https://statestoragetf.blob.core.windows.net/test-terraform-container/function-code.zip"
+# default="https://statestoragetf.blob.core.windows.net/test-terraform-container/function-code.zip"
 type = string
 }
